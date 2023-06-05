1ac5bed06379f3c99decf3ebe673468eba2c4265
Higlik <victor.silva.franca18@gmail.com>
2023-06-05 17:53:51 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace alura_movies_api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
