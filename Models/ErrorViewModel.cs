3858e8e831fbfcffabe5acbe1d7a14c8b7666d21
Higlik <victor.silva.franca18@gmail.com>
2023-06-05 18:13:02 -0300

    alter mvc to webapi

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace alura_movies_api.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
